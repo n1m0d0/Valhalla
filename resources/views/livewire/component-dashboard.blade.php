134ec18a7e354c8b3261fc2606fc00a423298283
n1m0d0 <33577927+n1m0d0@users.noreply.github.com>
2023-03-14 09:39:31 -0400

    traduccion de textos

--- resources/views/livewire/component-dashboard.blade.php
+++ resources/views/livewire/component-dashboard.blade.php
@@ -1,10 +1,10 @@
 <div class="grid grid-cols-1 md:grid-cols-12 gap-2">
-    <div class="col-span-1 md:col-span-3 p-2">
+    <div class="col-span-1 md:col-span-4 p-2">
         <div
             class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <div class="flex items-center justify-between mb-4">
                 <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">
-                    Latest Customers
+                    {{ __('Record') }}
                 </h5>
             </div>
             <div class="flow-root">
@@ -16,10 +16,10 @@
                             </div>
                             <div class="flex-1 min-w-0">
                                 <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    {{ __('Users') }}
+                                    {{ __('User') }}
                                 </p>
                                 <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                    {{ __('Registered users') }}
+                                    {{ __('Registered users.') }}
                                 </p>
                             </div>
                             <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
@@ -37,10 +37,10 @@
                             </div>
                             <div class="flex-1 min-w-0">
                                 <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    {{ __('Patients') }}
+                                    {{ __('Patient') }}
                                 </p>
                                 <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                    {{ __('Registered patients') }}
+                                    {{ __('Registered patients.') }}
                                 </p>
                             </div>
                             <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
@@ -58,10 +58,10 @@
                             </div>
                             <div class="flex-1 min-w-0">
                                 <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    {{ __('Meetings') }}
+                                    {{ __('Meeting') }}
                                 </p>
                                 <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                    {{ __('Registered meetings') }}
+                                    {{ __('Registered meetings.') }}
                                 </p>
                             </div>
                             <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
@@ -74,7 +74,7 @@
         </div>
     </div>
 
-    <div class="col-span-1 md:col-span-9 grid grid-cols-1 md:grid-cols-12 p-2 gap-2 items-center">
+    <div class="col-span-1 md:col-span-8 grid grid-cols-1 md:grid-cols-12 p-2 gap-2 items-center">
         <div class="border border-gray-200 dark:border-gray-700 rounded-lg shadow col-span-1 md:col-span-6 bg-white dark:bg-gray-800 h-64">
             
         </div>
